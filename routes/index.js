6ec14f91954e8aec8b1d6851b5cbd4b8f74bb42a
jmichaeljeffery <jmichael.jeffery@gmail.com>
2017-11-07 07:27:09 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 //########################################
+
 //to process data sent in on request need body-parser module
+
 var bodyParser = require('body-parser');
 var path = require ('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
@@ -11,13 +13,14 @@ router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
 
-// GET with  URI  /read/Lynne   which means name=Lynne
-router.get('/read/:name', function(req, res, next) {
+//now processing post
+router.post('/readNameAndRespond', function(req, res, next) {
+
     //expecting data variable called name --retrieve value using body-parser
     var body = JSON.stringify(req.body);  //if wanted entire body as JSON
     var params = JSON.stringify(req.params);//if wanted parameters
-    var value_name = req.params.name;  //retrieve the data associated with name
+    var value_name = req.body.name;  //retrieve the data associated with name
     res.send("hello " + value_name);
-})
+});
 
 module.exports = router;
