9640d28843b97536c134643673ccb02497324e65
jmichaeljeffery <jmichael.jeffery@gmail.com>
2017-11-09 17:09:00 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var XXX = express.Router();
+XXX.get('/mongodb', function (request, response) {
 
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+        //get collection of routes
+        var Routes = db.collection('Routes');
+        //get all Routes
+        Routes.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+
+});//end XXX.get
 //########################################
 
 //to process data sent in on request need body-parser module
